13b257fb73fea4e316b7cfce40726a83a23d0394
Devarakonda Muralikrishna <67271496+murali317@users.noreply.github.com>
2023-05-14 15:57:46 +0530

    Add files via upload
    This web app finds you weather of your desired place over the globe using an api from accuweather. You are sure want to check out the working source code that I've uploaded here.

---
+++ weather app/scripts/app.js
@@ -0,0 +1,64 @@
+const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img')
+
+const updateUI = (data) => {
+    // const cityDets = data.cityDets;
+    // const weather = data.weather; // alternative to this is to use destructuring 
+
+    // destructure properties
+    const {cityDets, weather} = data; // an easy way to get properties from an object and then store them in a constant of the same name.
+
+    // update details template
+    details.innerHTML = `
+        <h5 class="my-3">${cityDets.EnglishName}</h5>
+        <div class="my-3">${weather.WeatherText}</div>
+        <div class="display-4">
+            <span>${weather.Temperature.Metric.Value}</span>
+            <span>&deg;C</span>
+        </div>
+    `
+
+    // update the night/day & icon images
+    let timeSrc = null;
+    if (weather.IsDayTime){
+        timeSrc = 'img/day.svg'
+    }
+    else{
+        timeSrc = 'img/night.svg'
+    }
+    time.setAttribute('src', timeSrc)
+
+    if (card.classList.contains('d-none')){
+        card.classList.remove('d-none')
+    }
+
+    const icons = `img/icons/${weather.WeatherIcon}.svg`
+    icon.setAttribute('src', icons)
+
+}
+
+const updateCity = async (city) => {
+    const cityDets = await getCity(city);
+    const weather = await getWeather(cityDets.Key);
+
+    return {
+        cityDets : cityDets,
+        weather : weather
+    }
+}
+
+cityForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    // get city value
+    const city = cityForm.city.value.trim();
+    cityForm.reset(); // clears the input field to enter the next city
+
+    // update the ui with new city
+    updateCity(city)
+        .then(data => updateUI(data))
+        .catch(err => console.log(err))
+})
